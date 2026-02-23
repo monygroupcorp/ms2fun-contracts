ba87bb7ff1507808e3fa7ae90864fa188a2f20bb
jfkwebdev <jfkwebdev@gmail.com>
2026-02-23 05:47:28 -0500

    refactor: remove UniswapVaultSwapRouter from factory, take zRouter params + pre-deployed priceValidator

--- src/vaults/UltraAlignmentVaultFactory.sol
+++ src/vaults/UltraAlignmentVaultFactory.sol
@@ -3,48 +3,36 @@ pragma solidity ^0.8.20;
 
 import {LibClone} from "solady/utils/LibClone.sol";
 import {UltraAlignmentVault} from "./UltraAlignmentVault.sol";
-import {UniswapVaultSwapRouter} from "../peripherals/UniswapVaultSwapRouter.sol";
-import {UniswapVaultPriceValidator} from "../peripherals/UniswapVaultPriceValidator.sol";
-import {IVaultSwapRouter} from "../interfaces/IVaultSwapRouter.sol";
 import {IVaultPriceValidator} from "../interfaces/IVaultPriceValidator.sol";
 
 /// @title UltraAlignmentVaultFactory
-/// @notice Deploys UltraAlignmentVault clones with shared peripheral singletons
+/// @notice Deploys UltraAlignmentVault clones; zRouter config is shared across all vaults.
 contract UltraAlignmentVaultFactory {
     address public immutable vaultImplementation;
-    IVaultSwapRouter public immutable defaultSwapRouter;
     IVaultPriceValidator public immutable defaultPriceValidator;
 
     address public immutable weth;
     address public immutable poolManager;
-    address public immutable v3Router;
-    address public immutable v2Router;
-    address public immutable v2Factory;
-    address public immutable v3Factory;
+    address public immutable zRouter;
+    uint24  public immutable zRouterFee;
+    int24   public immutable zRouterTickSpacing;
 
     event VaultDeployed(address indexed vault, address indexed alignmentToken, address indexed creator);
 
     constructor(
         address _weth,
         address _poolManager,
-        address _v3Router,
-        address _v2Router,
-        address _v2Factory,
-        address _v3Factory
+        address _zRouter,
+        uint24  _zRouterFee,
+        int24   _zRouterTickSpacing,
+        IVaultPriceValidator _defaultPriceValidator
     ) {
         weth = _weth;
         poolManager = _poolManager;
-        v3Router = _v3Router;
-        v2Router = _v2Router;
-        v2Factory = _v2Factory;
-        v3Factory = _v3Factory;
-
-        defaultSwapRouter = new UniswapVaultSwapRouter(
-            _weth, _poolManager, _v3Router, _v2Router, _v2Factory, _v3Factory, 3000
-        );
-        defaultPriceValidator = new UniswapVaultPriceValidator(
-            _weth, _v2Factory, _v3Factory, _poolManager, 500
-        );
+        zRouter = _zRouter;
+        zRouterFee = _zRouterFee;
+        zRouterTickSpacing = _zRouterTickSpacing;
+        defaultPriceValidator = _defaultPriceValidator;
         vaultImplementation = address(new UltraAlignmentVault());
     }
 
@@ -52,14 +40,12 @@ contract UltraAlignmentVaultFactory {
     /// @param alignmentToken The token this vault aligns to
     /// @param factoryCreator Address that receives creator yield cut
     /// @param creatorYieldCutBps Creator yield cut in basis points (max 500)
-    /// @param swapRouter Custom swap router; uses defaultSwapRouter if address(0)
     /// @param priceValidator Custom price validator; uses defaultPriceValidator if address(0)
     /// @return vault Address of the deployed vault clone
     function deployVault(
         address alignmentToken,
         address factoryCreator,
         uint256 creatorYieldCutBps,
-        IVaultSwapRouter swapRouter,
         IVaultPriceValidator priceValidator
     ) external returns (address vault) {
         vault = LibClone.clone(vaultImplementation);
@@ -67,14 +53,12 @@ contract UltraAlignmentVaultFactory {
         UltraAlignmentVault(payable(vault)).initialize(
             weth,
             poolManager,
-            v3Router,
-            v2Router,
-            v2Factory,
-            v3Factory,
             alignmentToken,
             factoryCreator,
             creatorYieldCutBps,
-            swapRouter == IVaultSwapRouter(address(0)) ? defaultSwapRouter : swapRouter,
+            zRouter,
+            zRouterFee,
+            zRouterTickSpacing,
             priceValidator == IVaultPriceValidator(address(0)) ? defaultPriceValidator : priceValidator
         );
 
